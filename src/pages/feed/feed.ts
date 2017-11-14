0d4177c9e3afd560198287ae02d55c4010d65c54
DeveloperMatheus <matheus.developer@gmail.com>
2017-11-13 23:57:30 -0200

    Consumindo um JSON estático

--- src/pages/feed/feed.ts
+++ src/pages/feed/feed.ts
@@ -14,12 +14,22 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'feed.html',
 })
 export class FeedPage {
-/* restringindo o tipo de uma variável */
+  //Meu objeto em JSON que vai ser consumido na página de feed
+  public objeto_feed = {
+    titulo: "Um título de feed",
+    data: "5 de Novembro, 1955",
+    descricao: "Estou criando um app aqui usando JSON",
+    qntdLikes: 12,
+    qntdComentarios: 4,
+    tempoComentario: "11h atrás"
+  }
+
+  /* restringindo o tipo de uma variável */
   public nomeUsuario:string = "Matheus Menezes (código)";
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
-  
+
   /* função void sem retorno */
   public somaDoisNumeros(num1:number, num2:number):void {
     alert(num1 + num2);
